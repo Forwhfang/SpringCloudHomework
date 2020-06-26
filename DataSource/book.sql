23fe0dfac471d4e0f57e5362f9e9f34b32916826
Larrisa <1393317161@qq.com>
2020-06-27 00:10:53 +0800

    Update:update sql file

--- DataSource/book.sql
+++ DataSource/book.sql
@@ -4,7 +4,7 @@ create table tb_book(
     id int primary key NOT NULL AUTO_INCREMENT,
     imgUrl TEXT,
     name VARCHAR(255),
-    star DOUBLE(2,1),
+    star VARCHAR(20),
     quotes VARCHAR(255),
     author VARCHAR(255),
     translator VARCHAR(255),
